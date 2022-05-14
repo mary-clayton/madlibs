012b2250e9ac5f595089788300dcdbc2cd337f7e
hismrsayers <54823387+hismrsayers@users.noreply.github.com>
2022-05-13 18:12:40 -0700

    madlib1

---
+++ madlibs.py
@@ -0,0 +1,15 @@
+# string concatenation (aka how to put strings together)
+# youtuber = "Kylie Ying"
+# print("subscribe to" + youtuber)
+# print("Subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so {adj}! It's makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
